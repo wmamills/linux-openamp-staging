49733656ac05ed5e6b299d4900d9885b6db718a2
Bill Mills <bill.mills@linaro.org>
2022-05-26 19:15:08 -0400

    update.sh: fixups for a fresh repo
    * use a VAR for the upstream push repo
    * use full name for wam repo
    * remove rt*-{patches,rebase}
    * do initial push in stages to avoid github 2GB pack limit
    * push tags
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Update the various branches and tags
 
+PUSH_TO=wam
+
 add_remote() {
     if ! git remote get-url $1 >/dev/null 2>&1; then
         git remote add $1 $2
@@ -12,7 +14,7 @@ add_remote stable       https://git.kernel.org/pub/scm/linux/kernel/git/stable/l
 add_remote rt           https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git
 add_remote rt-stable    https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git
 add_remote remoteproc   https://git.kernel.org/pub/scm/linux/kernel/git/remoteproc/linux.git
-add_remote wam          git@github.com:wmamills/linux.git
+add_remote wam          git@github.com:wmamills/linux-openamp-staging.git
 
 git fetch --all
 
@@ -28,8 +30,21 @@ git tag -d latest
 # get rid of all -rc tags but keep the last 3 releases worth
 for i in $(git tag | grep "\-rc" | grep -v "v5.1[876]"); do git tag -d $i; done
 
+# get rid of rt-patches tags
+for i in $(git tag | grep "\rt.*-patches$" ); do git tag -d $i; done
+
+# get rid of rt-rebase tags
+for i in $(git tag | grep "\rt.*-rebase$" ); do git tag -d $i; done
+
 # optional, get rid of all word prefixed tags (would include next-*)
 for i in $(git tag | grep -v "^v"); do git tag -d $i; done
 
-git push wam linus/master:refs/heads/linus/master
-git push wam remoteproc/for-next:refs/heads/remoteproc/for-next
+# this is needed if the remote repo is new and on github
+# this makes the pack smaller so as not to exceed 2GB
+for i in v2.6.11 v3.0 v4.0 v5.0 v5.18; do
+    git push wam $i
+done
+
+git push $PUSH_TO linus/master:refs/heads/linus/master
+git push $PUSH_TO remoteproc/for-next:refs/heads/remoteproc/for-next
+git push $PUSH_TO --tags
