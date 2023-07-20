a4ca2d4ee986d85752c5ec306f19555f9a7e6462
Gatien Chevallier <gatien.chevallier@foss.st.com>
2023-07-20 18:00:55 +0200

    of: property: fw_devlink: Add support for "access-controller"
    Allows tracking dependencies between devices and their access
    controller.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Acked-by: Rob Herring <robh@kernel.org>

--- drivers/of/property.c
+++ drivers/of/property.c
@@ -1238,6 +1238,7 @@ DEFINE_SIMPLE_PROP(resets, "resets", "#reset-cells")
 DEFINE_SIMPLE_PROP(leds, "leds", NULL)
 DEFINE_SIMPLE_PROP(backlight, "backlight", NULL)
 DEFINE_SIMPLE_PROP(panel, "panel", NULL)
+DEFINE_SIMPLE_PROP(access_controllers, "access-controllers", "#access-controller-cells")
 DEFINE_SUFFIX_PROP(regulators, "-supply", NULL)
 DEFINE_SUFFIX_PROP(gpio, "-gpio", "#gpio-cells")
 
@@ -1346,6 +1347,7 @@ static const struct supplier_bindings of_supplier_bindings[] = {
 	{ .parse_prop = parse_regulators, },
 	{ .parse_prop = parse_gpio, },
 	{ .parse_prop = parse_gpios, },
+	{ .parse_prop = parse_access_controllers, },
 	{}
 };
 
