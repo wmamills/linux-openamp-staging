d73e51141c851f34e77fad9c8f611bcd98dc0554
Alain Volmat <alain.volmat@foss.st.com>
2024-10-03 10:13:21 +0530

    Revert "i2c: stm32f7: increase AUTOSUSPEND_DELAY to avoid lock"
    This reverts commit 1969a0ce85d126f48608fd85fa29c399209e55d9.
    
    With the introduction of commit
    'i2c: stm32f7: Do not prepare/unprepare clock during runtime
    suspend/resume',
    this commit is no more necessary.
    
    Change-Id: I179c7f3cee0293aac8a78b30e4c1a33a3c994a58
    Signed-off-by: Alain Volmat <alain.volmat@foss.st.com>
    Signed-off-by: Deepak Kumar <deepak.kumar01@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/linux-stm32/+/408695
    Reviewed-by: Alain VOLMAT <alain.volmat@st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Amelie DELAUNAY <amelie.delaunay@foss.st.com>
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    Domain-Review: Amelie DELAUNAY <amelie.delaunay@foss.st.com>

--- drivers/i2c/busses/i2c-stm32f7.c
+++ drivers/i2c/busses/i2c-stm32f7.c
@@ -180,12 +180,7 @@ enum {
 #define STM32F7_SCLH_MAX			BIT(8)
 #define STM32F7_SCLL_MAX			BIT(8)
 
-/*
- * This autosuspend delay should be long enough so that a clk client device performing i2c
- * transfer within its prepare_lock protected handler should be able to perform the whole
- * handling without having the autosuspend to try to suspend the i2c adapter
- */
-#define STM32F7_AUTOSUSPEND_DELAY		HZ
+#define STM32F7_AUTOSUSPEND_DELAY		(HZ / 100)
 
 /**
  * struct stm32f7_i2c_regs - i2c f7 registers backup
